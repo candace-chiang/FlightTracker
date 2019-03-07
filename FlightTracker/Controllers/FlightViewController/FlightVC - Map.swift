3148847813f3631ce747dbc269b85d4e9ed13159
Candace Chiang <chiang.candace@berkeley.edu>
2019-03-06 21:10:55 -0800

    nothing works dodododoooooo

--- FlightTracker/Controllers/FlightViewController/FlightVC - Map.swift
+++ FlightTracker/Controllers/FlightViewController/FlightVC - Map.swift
@@ -40,47 +40,7 @@ extension FlightViewController: MKMapViewDelegate {
         mapView.showAnnotations([depart, arrive], animated: true)
         let coordinates = [depart.coordinate, arrive.coordinate]
         mapView.addOverlay(MKGeodesicPolyline(coordinates: coordinates, count: coordinates.count))
-        /*
         
-        mapView.showAnnotations([source, destination], animated: true)
-        
-        var coordinates = [source.coordinate, destination.coordinate]
-        
-        let geodesicPolyline = MKGeodesicPolyline(coordinates: &coordinates, count: 2)
-        mapView.addOverlay(geodesicPolyline)
-        
-        
-        let paddingOfMap = 1.5
-        debugPrint(UIApplication.shared.statusBarFrame.maxY)
-        var latDelta = abs(destination.coordinate.latitude - source.coordinate.latitude) * paddingOfMap
-        let lonDelta = abs(destination.coordinate.longitude - source.coordinate.longitude) * paddingOfMap
-        
-        let percentToUse = (mapView.frame.height - heightOfPane)/mapView.frame.height
-        latDelta = Double(CGFloat(latDelta)/(percentToUse))
-        
-        if latDelta > 180 {
-            latDelta = 180
-        }
-        
-        let centerY = max(destination.coordinate.latitude,source.coordinate.latitude) - latDelta/2.8
-        let centerX = (destination.coordinate.longitude + source.coordinate.longitude)/2
-        
-        let spanny = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
-        
-        let centerman = CLLocationCoordinate2D(latitude: centerY, longitude: centerX)
-        
-        mapView.setRegion(MKCoordinateRegion(center: centerman, span: spanny), animated: true)
-        
-        
-    }
-    
-    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
-        let polylineRenderer = MKPolylineRenderer(overlay: overlay)
-        polylineRenderer.strokeColor = Constants.DEEP_BLUE
-        polylineRenderer.alpha = 0.9
-        polylineRenderer.lineWidth = 5
-        return polylineRenderer
-    }*/
     }
     
 }
